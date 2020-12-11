24f41fdbf2ec0e8306662cf3e54feef6a0156635
zdu863 <zdu863@gmail.com>
2020-12-10 21:26:44 -0800

    Day11

---
+++ Day11/2.py
@@ -0,0 +1,44 @@
+with open('input.txt','r') as f:
+    grid=f.read().splitlines()
+grid=[list(r) for r in grid]
+
+m,n=len(grid),len(grid[0])
+
+def occupied(grid,i,j):
+    ret=0
+    m,n=len(grid),len(grid[0])
+    for di,dj in [(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)]:
+        I,J=i+di,j+dj
+        while 0<=I<m and 0<=J<n:
+            if grid[I][J]=='#':
+                ret+=1
+                break
+            elif grid[I][J]=='L':
+                break
+            I+=di
+            J+=dj
+    return ret
+
+while True:
+    stable=True
+    grid_c=[r.copy() for r in grid]
+    for i,r in enumerate(grid_c):
+        for j,c in enumerate(r):
+            count=occupied(grid_c,i,j)
+            if c=='L' and count==0:
+                grid[i][j]='#'
+            elif c=='#' and count>=5:
+                grid[i][j]='L'
+        stable&=(grid[i]==grid_c[i])
+    if stable:
+        break
+        
+ans=0
+for i in range(m):
+    for j in range(n):
+        if grid[i][j]=='#':
+            ans+=1
+print(ans)
+    
+        
+    
