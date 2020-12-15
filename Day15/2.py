d65c482a4884e9f5cdb67a3c0e0246cd9a3cdcaf
zdu863 <zdu863@gmail.com>
2020-12-14 21:25:26 -0800

    Day15

---
+++ Day15/2.py
@@ -0,0 +1,17 @@
+arr=[7,12,1,0,16,2]
+# arr=[1,3,2]
+
+d={a:[i] for i,a in enumerate(arr)}
+
+lst=arr[-1]
+for i in range(len(arr),30000000):
+    if len(d[lst])==1:
+        spoken=0
+    else:
+        spoken=d[lst][-1]-d[lst][-2]
+    if spoken in d:
+        d[spoken].append(i)
+    else:
+        d[spoken]=[i]
+    lst=spoken
+print(spoken)
